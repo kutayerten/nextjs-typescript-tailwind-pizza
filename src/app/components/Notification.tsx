47d6c84258e95b08859c6579de7d00c5e3dd19fd
kutay <kutayerten@gmail.com>
2023-12-04 22:51:01 +0300

    vercel deploy error

--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+
 
 const Notification = () => {
   return (
-    <div className='h-12 bg-green-500 text-white flex items-center justify-center px-4'>First Pizza You Got %30 Sale. Don't waste it !!!</div>
+    <div className="h-12 bg-green-500 text-white flex items-center justify-center px-4">First Pizza You Got %30 Sale</div>
   )
 }
 
